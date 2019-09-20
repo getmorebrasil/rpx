a44f4d56a2d8bb081a71a53410ccf477bbddb3d4
William Müller <william-code@hotmail.com>
2019-09-20 12:31:54 -0300

    altered rpc_Elixir to rpx in mix.ex file

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RpcElixir.MixProject do
 
   def project do
     [
-      app: :rpc_elixir,
+      app: :rpx,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
