43a83dacf417558d0bbe4e085b8155f2cfeca72d
Bruno Hass <bruno_hass@live.com>
2019-10-20 19:51:02 -0300

    Adds travis-ci file

--- test/rpx_test.exs
+++ test/rpx_test.exs
@@ -3,16 +3,16 @@ defmodule ServerTest do
   doctest Server
 
   test "A new server should be created" do
-    assert Server.new("Test", MockConnection, MockConnection.new("protocol://a")) == %Server{
+    assert Server.new("Test", MockProtocol, MockProtocol.new("protocol://a")) == %Server{
       name: "Test",
-      connection_handler: MockConnection,
+      connection_handler: MockProtocol,
       connection_data: %{},
       procedures: %{}
     }
   end
 
   test "A function should be added to the server" do
-    server = Server.new("Test", MockConnection, MockConnection.new("protocol://a"))
+    server = Server.new("Test", MockProtocol, MockProtocol.new("protocol://a"))
     |> Server.add_procedure(&MockWorker.some_function/2)
 
     assert Map.has_key?(server.procedures, :some_function)
